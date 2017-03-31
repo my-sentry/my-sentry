2f1038ffb3813044f13d213ab231dded2f06a050
universalmind303 <universalmind.candy@gmail.com>
2017-03-31 13:45:46 -0500

    (Bugfix) removed duplicates

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,3 @@
-import { combineReducers } from 'redux';
-
-function createReducer(initialState, handlers) {
-  return function reducer(state = initialState, action) {
-    if (handlers.hasOwnProperty(action.type)) {
-      return handlers[action.type](state, action);
-    } else {
-      return state;
-    }
-  };
-}
-
-const Reducer = createReducer([], {
-  // some reduced func
-});
-
 import {combineReducers} from 'redux';
 
 function createReducer(initialState, handlers) {
