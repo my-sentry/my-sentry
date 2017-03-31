2368c47a69d82fc421a9db3dd7f00ceba2452006
universalmind303 <universalmind.candy@gmail.com>
2017-03-31 16:31:13 -0500

    (Enhancement) extracted mocks to unique location

--- tests/client/dashboard.test.js
+++ tests/client/dashboard.test.js
@@ -8,16 +8,6 @@ import ReactDOM from 'react-dom';
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
-jest.mock('Linking', () => {
-  return {
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    openURL: jest.fn(),
-    canOpenURL: jest.fn(),
-    getInitialURL: jest.fn(),
-  };
-});
-
 describe('app.js', function() {
   var app;
   it('renders correctly', () => {
