37b878998676e2b584b0487a3f2b72c33c9ee561
universalmind303 <universalmind.candy@gmail.com>
2017-03-31 13:42:56 -0500

    (Bugfix) index.android/ios.js

--- index.ios.js
+++ index.ios.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import MySentry from './src/index.js'
+import MySentry from './src/app.js';
 
 
 AppRegistry.registerComponent('mysentry', () => MySentry);
