2a38e5760c251d028ad9a9fb1d0f2fd4e4ea696f
Jerry Krusinski <jkruzr@gmail.com>
2017-04-02 13:41:49 -0500

    wrote groupCtrl describe and it expressions

---
+++ tests/server/controllers/groupCtrlSpec.js
@@ -0,0 +1,50 @@
+const expect = require('chai').expect;
+const groups = require('../../server/db/controllers/groupCtrl.js');
+
+describe('Group Controller', () => {
+
+  describe('createGroup', () => {
+
+    it('should add a group to the database', () => {
+
+    });
+
+    it('should make the user who created the group admin_user', () => {
+
+    });
+
+  });
+
+  describe('getGroupById', () => {
+
+    it('should retrieve a group from the databse', () => {
+
+    });
+
+  });
+
+  describe('getGroups', () => {
+
+    it('should retrive a list of groups specific to the user', () => {
+
+    });
+
+  });
+
+  describe('updateGroupById', () => {
+
+    it('should update a group in the database', () => {
+
+    });
+
+  });
+
+  describe('deleteGroupById', () => {
+
+    it('should delete a group in the database', () => {
+
+    });
+
+  });
+
+});
