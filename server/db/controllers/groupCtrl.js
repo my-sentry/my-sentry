5b135bf632f4ee5bc61c9359461a8c057f1f79cd
Jerry Krusinski <jkruzr@gmail.com>
2017-04-05 12:54:01 -0500

    created get groups a user can see and get group with members

--- server/db/controllers/groupCtrl.js
+++ server/db/controllers/groupCtrl.js
@@ -15,14 +15,49 @@ exports.createGroup = function(name, adminUser) {
 exports.getGroupById = function (id) {
   return knex('groups')
     .where('id', id)
-    .then(result => result[0]);
+    .then(result => result[0])
+    .then(group => {
+
+      return knex('users')
+        .select('users.*')
+        .innerJoin('groups', function() {
+          this.on('users.id', 'groups.admin_user')
+            .andOn('groups.id', knex.raw(id));
+        }).union(function() {
+          this.select('users.*')
+            .from('users')
+            .innerJoin('group_user', 'group_user.user_id', 'users.id')
+            .innerJoin('groups', function() {
+              this.on('group_user.group_id', 'groups.id')
+                .andOn('groups.id', knex.raw(id));
+            });
+        })
+
+        .then(users => {
+          group.users = users;
+          return group;
+        });
+      
+    });
+
 };
 
-//Get groups
-exports.getGroups = function () {
-  return knex.select()
-    .from('groups')
-    .then(results => results);
+//Gets all groups a user is a part of
+exports.getGroups = function (username) {
+  return knex('groups')
+    .select('groups.*')
+    .innerJoin('users', function() {
+      this.on('users.id', 'groups.admin_user')
+        .andOn('users.username', knex.raw(`'${username}'`));
+    }).union(function() {
+      this.select('groups.*')
+        .from('groups')
+        .innerJoin('group_user', 'groups.id', 'group_user.group_id')
+        .innerJoin('users', function() {
+          this.on('users.id', 'group_user.user_id')
+            .andOn('users.username', knex.raw(`'${username}'`));
+        });
+    });
 };
 
 //Update group
