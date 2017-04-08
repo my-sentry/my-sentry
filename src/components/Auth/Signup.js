d51dc17d5a15b347d4c46d41ca78aa9afde05f1d
universalmind303 <universalmind.candy@gmail.com>
2017-04-08 14:28:13 -0500

    signup rerouting to dashboard properly, more codecleanup

--- src/components/Auth/Signup.js
+++ src/components/Auth/Signup.js
@@ -3,7 +3,7 @@ import {Actions} from 'react-native-router-flux';
 import Header from './authHeader';
 import { connect } from 'react-redux';
 import axios from 'axios';
-import { Container, Title, Content, Label, Form, Button, Item, Text, Icon, Right, Body, Input, H1, Grid, Row } from 'native-base';
+import { Container, Title, Content, Label, Form, Button, Item, Text, Icon, Left, Right, Body, Input, H1, Grid, Row } from 'native-base';
 
 const styles = {
   centering: {
@@ -58,9 +58,12 @@ export default connect(mapStateToProps)(function Login ({signup, dispatch}) {
     </Content>
       </Row>
       <Row style={{flex: 0}}>
-        <Button block onPress={Actions.login}>
+        <Left>
+        <Button outline bordered onPress={Actions.login}>
           <Icon name='arrow-back'/>
         </Button>
+        </Left>
+      <Right>
       <Button block onPress={() => {
 
         let data = JSON.stringify({
@@ -80,19 +83,18 @@ export default connect(mapStateToProps)(function Login ({signup, dispatch}) {
           url: 'http://192.168.1.163:8000/api/users/signup',
           data: data
         }).then(res => {
-          setTimeout(() => Actions.loading());
-          dispatch({type: 'LOGIN'});
+          dispatch({type: 'LOGIN', id: res.data.id});
           dispatch({type: 'CLEAR_SIGNUP'});
-        })
-          .catch(err => {
-            setTimeout(() => alert('invalid submission'));
-            dispatch({type: 'CLEAR_SIGNUP'});
-          })
+        }).catch(err => {
+          setTimeout(() => alert('invalid submission'));
+          dispatch({type: 'CLEAR_SIGNUP'});
+        }).then(() => Actions.loading())
         : alert('passwords dont match');
 
       }}>
         <Text> Create Account</Text>
       </Button>
+      </Right>
     </Row>
     </Grid>
   </Container>
