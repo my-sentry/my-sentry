add3252a584e78d4cc50776aa68895ea19f1e844
Jerry Krusinski <jkruzr@gmail.com>
2017-04-10 15:00:35 -0500

    added token column to database

---
+++ migrations/20170410145630_token.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.string('token').default('');
+  });
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.table('users', function(table) {
+    table.dropColumn('token');
+  });
+};
