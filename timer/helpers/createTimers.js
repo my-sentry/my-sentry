ddaec4846e4d0dad0095fcb3dbd665774402c4ed
Jerry Krusinski <jkruzr@gmail.com>
2017-04-11 17:21:40 -0500

    tied timers to push notifications

--- timer/helpers/createTimers.js
+++ timer/helpers/createTimers.js
@@ -1,27 +1,23 @@
 var timers = require('../../server/db/controllers/timersCtrl');
 
-var MS_PER_MINUTE = 60000;
-
-var types = ['warning10', 'warning2', 'danger'];
-
-var offsets = {
-  warning10: 10,
-  warning2: 2,
-  danger: 0
-};
+var {
+  MS_PER_MINUTE,
+  TIMER_TYPES,
+  TIMER_OFFSETS
+} = require('./constants');
 
 var offsetMinutes = function(offset, date) {
   return new Date(date - (offset * MS_PER_MINUTE));
 };
 
 module.exports = function(event) {
-  var insertTimers = types
+  var insertTimers = TIMER_TYPES
     // create each timer to insert into db
     .map(type => {
       return {
         'event_id': event.id,
         'type': type,
-        'time': offsetMinutes(offsets[type], new Date(event.end))
+        'time': offsetMinutes(TIMER_OFFSETS[type], new Date(event.end))
       };
     })
     // wrap timers with db insert Promises
--- timer/test.js
+++ timer/test.js
@@ -3,12 +3,12 @@ var client = require('./client');
 client.initialize();
 
 client.sendEvent({
-  id: 2,
-  'user_id': 3,
+  id: 1,
+  'user_id': 1,
   'group_id': 2,
   name: 'Austonian Showing',
   begin: '2017-04-11 12:00:00 GMT-0500 (CDT)',
-  end: '2017-04-11 16:50:00 GMT-0500 (CDT)',
+  end: '2017-04-11 17:30:00 GMT-0500 (CDT)',
   lat: 30.264562,
   long: -97.744294,
   description: 'Showing an apartment at the Austonian. The contact is 5129384898.'
