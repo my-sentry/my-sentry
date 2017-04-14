dbd0ca600d5c0cf83e5049b56698232205e438f4
Jerry Krusinski <jkruzr@gmail.com>
2017-04-14 11:44:46 -0500

    added service skeleton for timer updates

--- timer/service.js
+++ timer/service.js
@@ -13,11 +13,19 @@ var sendErr = function(socket, err) {
 
 ipc.serve(() => {
 
-  ipc.server.on('event', (data, socket) => {
+  ipc.server.on('create', (data, socket) => {
     var event = JSON.parse(data);
     createTimers(event.id, event.end).then(ids => {
       console.log('Created timers: ', ids);
     });
+  });
+
+  ipc.server.on('safe', (data, socket) => {
+    var eventId = JSON.parse(data);
+  });
+
+  ipc.server.on('danger', (data, socket) => {
+    var eventId = JSON.parse(data);
   });
 
 });
